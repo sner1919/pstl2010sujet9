6e5cefb1d68968cbe088d138f2a9b72ee670304a
mikael.cacote <mikael.cacote@d2a9bb46-1fae-11df-a4a7-ed6ba186e60d>
2010-02-23 02:45:07 +0000

    Il faut dans FCM arriver à appeler les super-constructeurs de la façon suivante : CCM(RCM(M))

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -9,10 +9,10 @@ template <class T> class Calculator : public ICalculator<T> {
 			if(!(A.getN() == B.getM()
 				 && Res.getM() == A.getM() && Res.getN() == B.getN())) throw domain_error("produit impossible");
 
-			for(int i = 0; i < Res.getM(); i++){
-				for(int j = 0; j < Res.getN(); j++){
+			for(int i = 1; i <= Res.getM(); i++){
+				for(int j = 1; j <= Res.getN(); j++){
 					Res(i, j) = 0.;
-					for(int k = 0; k < Res.getN(); k++) Res(i, j) = Res(i, j) + A(i, k) * B(k, j);
+					for(int k = 1; k <= Res.getN(); k++) Res(i, j) = Res(i, j) + A(i, k) * B(k, j);
 				}
 			}
 		}
@@ -21,8 +21,8 @@ template <class T> class Calculator : public ICalculator<T> {
 			// Vérifications
 			if(!(Res.getM() == A.getM() && Res.getN() == A.getN())) throw domain_error("produit impossible");
 
-			for(int i = 0; i < Res.getM(); i++){
-				for(int j = 0; j < Res.getN(); j++){
+			for(int i = 1; i <= Res.getM(); i++){
+				for(int j = 1; j<= Res.getN(); j++){
 					Res(i, j) = A(i, j) * x;
 				}
 			}
@@ -33,8 +33,8 @@ template <class T> class Calculator : public ICalculator<T> {
 			if(!(A.getM() == B.getM() && A.getM() == Res.getM()
 				 && A.getN() == B.getN() && A.getN() == Res.getN()))  throw domain_error("addition impossible");
 
-			for(int i = 0; i < Res.getM(); i++){
-				for(int j = 0; j < Res.getN(); j++){
+			for(int i = 1; i <= Res.getM(); i++){
+				for(int j = 1; j <= Res.getN(); j++){
 					Res(i, j) = 0.;
 					Res(i, j) = A(i, j) + B(i, j);
 				}
@@ -45,8 +45,8 @@ template <class T> class Calculator : public ICalculator<T> {
 			// Vérifications
 			if(!(A.getM() == Res.getN() && A.getN() == Res.getM()))  throw domain_error("transposition impossible");
 
-			for(int i = 0; i < Res.getM(); i++){
-				for(int j = 0; j < Res.getN(); j++){
+			for(int i = 1; i <= Res.getM(); i++){
+				for(int j = 1; j <= Res.getN(); j++){
 					Res(j, i) = A(i, j);
 				}
 			}
@@ -60,22 +60,26 @@ template <class T> class Calculator : public ICalculator<T> {
 				 && A.getM() == U.getM() && A.getM() == U.getN()))  throw domain_error("décomposition LU impossible");
 
 			T** c = new T*[A.getM()];
-			for(int i = 0; i < A.getM(); i++){
-				c[i] = new T[A.getM()];
-				for(int j = 0; j < A.getM(); j++) c[i][j] = A(i, j);
+			for(int i = 1; i <= A.getM(); i++){
+				c[i-1] = new T[A.getM()];
+				for(int j = 1; j <= A.getM(); j++) c[i-1][j-1] = A(i, j);
 			}
 
-			for(int k = 0; k < A.getM(); k++){
-				for(int i = 0; i < A.getM(); i++){
+			for(int k = 1; k <= A.getM(); k++){
+				for(int i = 1; i <= A.getM(); i++){
 					if(k > i) U(k, i) = 0;
-					else U(k, i) = c[k][i];
+					else U(k, i) = c[k-1][i-1];
 
 					if(i < k) L(i, k) = 0;
 					else if(i == k) L(i, k) = 1;
-					else L(i, k) = c[i][k] * (1 / U(k, k));
+					else L(i, k) = c[i-1][k-1] * (1 / U(k, k));
 
-					for(int j = 0; j < A.getM(); j++) c[i][j] = c[i][j] + L(i, k) * -U(k, j);
+					for(int j = 1; j <= A.getM(); j++) c[i-1][j-1] = c[i-1][j-1] + L(i, k) * -U(k, j);
 				}
 			}
+
+			// liberation de la mémoire
+			for(int i = 0; i < A.getM(); i++) delete [] c[i];
+			delete [] c;
 		}
 };
--- src/FullChecksumMatrix.cpp
+++ src/FullChecksumMatrix.cpp
@@ -1,7 +1,17 @@
-/*
- * FullChecksumMatrix.cpp
- *
- *  Created on: 21 févr. 2010
- *      Author: mikael
- */
+#pragma once
+#include "IFullChecksumMatrix.hpp"
+#include "ColumnChecksumMatrix.cpp"
+#include "RowChecksumMatrix.cpp"
+#include "Vector.cpp"
+#include <stdlib.h>
 
+template <class T> class FullChecksumMatrix : public RowChecksumMatrix<T>, public ColumnChecksumMatrix<T>, public IFullChecksumMatrix<T> {
+	public:
+		FullChecksumMatrix(IMatrix<T>& M) : Matrix<T>(M.getData(), M.getM() + 1, M.getN() + 1), RowChecksumMatrix<T>(M), ColumnChecksumMatrix<T>((*this)) {
+
+		}
+
+        T& operator()(int i, int j) {
+            return ColumnChecksumMatrix<T>::operator()(i, j);
+        }
+};
