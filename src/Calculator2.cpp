7e969aa8119a4155bcc7fded35fae66cd75bf874
georgesabouhaydar <georgesabouhaydar@d2a9bb46-1fae-11df-a4a7-ed6ba186e60d>
2010-02-28 23:25:16 +0000

    Version du calculateur avec la bibliothèque Lapack

---
+++ src/Calculator2.cpp
@@ -0,0 +1,60 @@
+#include "ICalculator.hpp"
+#include <sstream>
+#include <stdexcept>
+#include <iostream>
+#include "clapack.h"
+extern "C" {
+#include "cblas.h"
+}
+
+
+template <class T> class Calculator2 : public ICalculator<T> {
+	public:
+		int* ipvt;
+		int info;
+
+
+		void mult(IMatrix<T>& Res, IMatrix<T>& A, IMatrix<T>& B){
+			if(!(A.getN() == B.getM()
+							 && Res.getM() == A.getM() && Res.getN() == B.getN())) throw domain_error("produit impossible");
+			cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+					A.getM(), B.getN(), A.getN(), 1.0, A.getData(), A.getM(),
+					B.getData(), B.getM(), 1.0, Res.getData(), Res.getM());
+		}
+
+		void mult(IMatrix<T>& Res,IMatrix<T>& A, T x){
+			// Vérifications
+			if(!(Res.getM() == A.getM() && Res.getN() == A.getN())) throw domain_error("produit impossible");
+
+			cblas_dgemv(CblasColMajor, CblasNoTrans,A.getM(),A.getN(),
+					1.0,A.getData(),A.getM(),&x,0,0.0,Res.getData(),0);
+		}
+
+		void add(IMatrix<T>& Res,IMatrix<T>& A, IMatrix<T>& B){
+			dgeadd_(A.getData(),A.getM(),CblasNoTrans,B.getData(),B.getM(),
+					CblasNoTrans,Res.getData(),Res.getM(),Res.getM(),Res.getN());
+		}
+
+		void transpose(IMatrix<T>& Res, IMatrix<T>& A){
+			// Vérifications
+			if(!(A.getM() == Res.getN() && A.getN() == Res.getM()))  throw domain_error("transposition impossible");
+
+			cblas_dgemv(CblasColMajor, CblasTrans,A.getM(),A.getN(),
+					1.0,A.getData(),A.getM(),1.,0,0.0,Res.getData(),0);
+		}
+
+		// Hypothèse la matrice A est LU décomposable
+		void LU(IMatrix<T>& L, IMatrix<T>& U, IMatrix<T>& A){
+			// Vérifications
+			if(!(A.getM() == A.getN()
+				 && A.getM() == L.getM() && A.getM() == L.getN()
+				 && A.getM() == U.getM() && A.getM() == U.getN()))  throw domain_error("décomposition LU impossible");
+			ipvt = new int[A.getM()];
+			mult(U,A,1.);
+			dgetrf_(U.getM(),U.getN(),U.getData(),U.getM(),&ipvt, &info);
+			for(int i= 0;i < A.getM(); i++){
+				cout << ipvt[i] << " ";
+			}
+
+		}
+};
