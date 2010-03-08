e427a93c8911a6e720303f91f3f017cd03967136
georgesabouhaydar <georgesabouhaydar@d2a9bb46-1fae-11df-a4a7-ed6ba186e60d>
2010-03-08 23:02:40 +0000

    algorithme de correction pour une erreur d'autre cas sont aussi a faire

--- src/FullChecksumMatrix.cpp
+++ src/FullChecksumMatrix.cpp
@@ -3,6 +3,8 @@
 #include "RowChecksumMatrix.cpp"
 #include "ColumnChecksumMatrix.cpp"
 #include <stdlib.h>
+#include <vector>
+using std::vector;
 
 template <class T> class FullChecksumMatrix : public virtual Matrix<T>, public IFullChecksumMatrix<T> {
 		IRowChecksumMatrix<T>* RCM;
@@ -38,6 +40,41 @@ template <class T> class FullChecksumMatrix : public virtual Matrix<T>, public I
         }
 
         void errorCorrection() {
+        	IVector<T> *rowVector = &getRowSummationVector();
+        	IVector<T> *colVector = &getColumnSummationVector();
+        	vector<int> erreurlignes;
+        	vector<int> erreurcolonnes;
+        	if((computeRowSum(this.getM() != colVector->operator ()(this.getN())))
+        	        		|| (computeColumnSum(this.getN() != colVector->operator ()(this.getN())))){
 
+        	        	}
+        	for(int i = 0; i < this.getM()-1;i++)
+        	{
+        		if(computeRowSum(i) != rowVector->operator ()(i)){
+        			if((computeColumnSum(this.getN() != colVector->operator ()(this.getN()))))
+        			{
+        				rowVector->operator ()(i) = computeRowSum(i);
+        			}
+        			else{
+        				erreurlignes.push_back(i);
+        			}
+        		}
+        	}
+        	for(int j = 0; j < this.getN()-1;j++)
+        	{
+          		if(computeColumnSum(j) != colVector->operator ()(i)){
+        	        if((computeRowSum(this.getM() != colVector->operator ()(this.getN()))))
+        	        {
+        	        	colVector->operator ()(i) = computeColumnSum(i);
+        	        }
+        	        else{
+        	        	erreurcolonnes.push_back(i);
+        	       	}
+        	    }
+        	}
+        	if(erreurlignes.size() == 1){
+        		(*this)(erreurlignes[0],erreurscolonnes[0]) +=
+        				computeColumnSum(erreurscolonnes[0]) - colVector -> operator ()(erreurscolonnes[0]);
+        	}
         }
 };
