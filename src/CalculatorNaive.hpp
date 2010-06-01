681556d3b4b78c0ec945029054567f0c9883b719
georgesabouhaydar <georgesabouhaydar@d2a9bb46-1fae-11df-a4a7-ed6ba186e60d>
2010-06-01 07:14:06 +0000

    Implantation de l'algorithme de Strassen pour le produit de deux matrices (il subsiste encore quelques erreurs à corriger)

--- src/CalculatorNaive.hpp
+++ src/CalculatorNaive.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "settings.hpp"
 #include "interfaces/ICalculator.hpp"
+#include <cmath>
 
 template <class T> class CalculatorNaive : public virtual ICalculator<T> {
 	public:
@@ -26,3 +27,8 @@ template <class T1, class T2, class T3> void CalculatorNaiveMult(IMatrix<T1>& Re
 template <class T1, class T2, class T3> void CalculatorNaiveAdd(IMatrix<T1>& Res, const IMatrix<T2>& A, const IMatrix<T3>& B);
 template <class T1, class T2, class T3> void CalculatorNaiveTranspose(IMatrix<T1>& Res, const IMatrix<T2>& A);
 template <class T1, class T2, class T3, class T4> void CalculatorNaiveLU(IMatrix<T1>& P, IMatrix<T2>& L, IMatrix<T3>& U, const IMatrix<T4>& A);
+template <class T1, class T2> void CalculatorNaiveAddBlock(IMatrix<T1>& Res, const IMatrix<T2>& A, int x, int y, int i, int j, int N);
+template <class T1, class T2> void CalculatorNaiveSubBlock(IMatrix<T1>& Res, const IMatrix<T2>& A, int x, int y, int i, int j, int N);
+template <class T1, class T2> void CalculatorNaiveCopyBlock(IMatrix<T1>& Res, const IMatrix<T2>& A, int x, int y, int N);
+template <class T1, class T2, class T3> void CalculatorNaiveMult2(IMatrix<T1>& Res, const IMatrix<T2>& A, const IMatrix<T3>& B);
+template <class T1, class T2, class T3> void Strassen(IMatrix<T1>& Res, const IMatrix<T2>& A, const IMatrix<T3>& B, int N);
